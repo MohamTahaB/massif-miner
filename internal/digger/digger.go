62d1a76efd1869ff9f4e00a4f072edab8b2d7835
Mohamed Taha Bayoumi <bayoumimt@gmail.com>
2024-07-16 14:25:04 +0000

    Add digger site wrappers

--- internal/digger/digger.go
+++ internal/digger/digger.go
@@ -1,9 +1,35 @@
 package digger
 
-import "bufio"
+import (
+	"bufio"
+	"io"
 
+	"github.com/MohamTahaB/massif-miner/internal/outlog"
+)
+
+// A struct that wraps around a bufio scanner, in order to define member funcs and be able to add snapshots sequentially
 type DiggerSite struct {
 	Scanner *bufio.Scanner
 }
 
+// Initiates a digger site instance, from an io reader passed as input
+func InitDiggerSite(r io.Reader) DiggerSite {
+	return DiggerSite{
+		Scanner: bufio.NewScanner(r),
+	}
+}
 
+// Advances the digger site scanner to the next token.
+func (dg *DiggerSite) Scan() bool {
+	return dg.Scanner.Scan()
+}
+
+// Returns the most recent digger site token as a newly allocated string
+func (dg *DiggerSite) Text() string {
+	return dg.Scanner.Text()
+}
+
+// Sets the digger site meta data in the input outlog. Outputs a non nil error when the digger site token does not conform to the meta data section of a massif log file
+func (dg *DiggerSite) MetaData(log *outlog.OutLog) error {
+
+}
